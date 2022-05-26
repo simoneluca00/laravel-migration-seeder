ea0454c1b1f3d931da576148098ab43d7a5ac024
simoneluca00 <si.zaino@libero.it>
2022-05-26 15:31:55 +0200

    Migration Train created

---
+++ database/migrations/2022_05_26_131924_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->decimal('orario_di_partenza', 4,2);
+            $table->decimal('orario_di_arrivo', 4,2);
+            $table->string('codice_treno', 6);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
